05b28a8e84423fd0b15039a1943e94afe8f964c1
melih <melih@CNKVK>
2024-07-30 09:55:38 +0300

    Add project files.

---
+++ ElsaYazilim/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ElsaYazilim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
